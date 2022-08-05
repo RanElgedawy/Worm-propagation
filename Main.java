801fb54b271592dca24643831b86d1b5df7d5183
RanElgedawy <95228400+RanElgedawy@users.noreply.github.com>
2022-08-05 15:41:23 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package wormpropagation1;
+package wormpropagation2;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -28,6 +28,13 @@ public class Main {
         Vulnerable,
         Infected
     }
+    
+    public enum nodeScanType 
+    {
+        Immune,
+        Random,
+        Sequential
+    }
 
     public static class node {
 
@@ -36,6 +43,7 @@ public class Main {
         public nodeType nodestatus;
         public nodeType nodepriorstatus;
         public int nodestarttime;
+        public nodeScanType nodescantype;
     }
 
     public static void main(String[] args) throws IOException {
@@ -45,10 +53,13 @@ public class Main {
         node[] nodes = new node[IPSpace + 1];
         Integer[][] It = new Integer[simulN+1][simulTime+1];
         //int numvulnerable = 1000;
-        Random rand = new Random(); 
+       
         int randomip = 0;  
         int infectedNum=0;
-         FileWriter fw = new FileWriter("out.txt");
+        int nextip = 0;
+         Random rand = new Random(); 
+        Random rand1 = new Random();
+         FileWriter fw = new FileWriter("out2.txt");
         //while (true)
         //{
         //initializing all nodes
@@ -60,6 +71,7 @@ public class Main {
             for (int i = 1; i <= IPSpace; i++) {
                     nodes[i].nodestatus = nodeType.Immune;
                     nodes[i].nodepriorstatus = nodeType.Immune;
+                    nodes[i].nodescantype = nodeScanType.Immune;
                 }
                 for (int j = 0; j <= 9; j++) {
                     for (int i = 1; i <= 100; i++) {
@@ -72,6 +84,15 @@ public class Main {
             nodes[10050].nodestatus = nodeType.Infected;
             nodes[10050].nodepriorstatus = nodeType.Infected; //t=0
             nodes[10050].nodestarttime = 1;
+            double x = rand1.nextDouble();
+            if (x <= 0.8)
+            {
+                nodes[10050].nodescantype = nodeScanType.Sequential;
+            }
+            else
+            {
+                nodes[10050].nodescantype = nodeScanType.Random;
+            }
             It[n][t]=1;     
        
         
@@ -84,10 +105,19 @@ public class Main {
                 for (int i = 1; i <= IPSpace; i++){
                 if (nodes[i].nodepriorstatus.equals(nodeType.Infected) && nodes[i].nodestarttime<=t)
                 {
-                    
+                     double x1 = rand1.nextDouble();
+            if (x1 <= 0.8)
+            {
+                nodes[i].nodescantype = nodeScanType.Sequential;
+            }
+            else
+            {
+                nodes[i].nodescantype = nodeScanType.Random;
+            }
+                 if (nodes[i].nodescantype.equals(nodeScanType.Random)){   
                      for (int k=0; k<5;k++)
                 {
-                    
+                   
                     randomip = rand.nextInt(IPSpace) + 1;
                     //System.out.println(randomip);
                     if (nodes[randomip].nodepriorstatus.equals(nodeType.Vulnerable) && nodes[randomip].nodestatus != nodeType.Infected)
@@ -95,13 +125,37 @@ public class Main {
                         nodes[randomip].nodestatus = nodeType.Infected;
                         nodes[randomip].nodestarttime = t+10;
                         infectedNum++;
-                        System.out.print("randomip = " + randomip);
-                        System.out.print("node status" + nodes[randomip].nodestatus);
-                        System.out.println("infected number = " + infectedNum);
+              
+                        //System.out.println("infected number = " + infectedNum);
                         It[n][t]++;
                     }
                     
                 }
+                }
+                 else
+                 {
+                       for (int k=1; k<=5;k++)
+                {
+                   
+                    if (i != 100000){
+                    nextip = i+k;}
+                    else
+                    {
+                      nextip = 1;  
+                    }
+                    
+                    //System.out.println(randomip);
+                    if (nodes[nextip].nodepriorstatus.equals(nodeType.Vulnerable) && nodes[nextip].nodestatus != nodeType.Infected)
+                    {
+                        nodes[nextip].nodestatus = nodeType.Infected;
+                        nodes[nextip].nodestarttime = t+10;
+                        infectedNum++;
+              
+                        //System.out.println("infected number = " + infectedNum);
+                        It[n][t]++;
+                    }
+                }
+                 }
                 }
                 }
                 
